dfcdff75943853b54844ef94d5492319294197dc
Olly Shaw <ollyjshaw@gmail.com>
2017-02-10 13:48:47 +0000

    Added a base project

---
+++ Echo.cs
@@ -0,0 +1,10 @@
+using System;
+
+namespace Tests
+{
+  public class Echo {
+    public String echo(string toEcho){
+      return toEcho;
+    }
+  }
+}
---
+++ Tests.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Xunit;
+using System.Collections.Generic;
+
+namespace Tests
+{
+    public class Tests
+    {
+      [Fact]
+      public void MathsWorks()
+      {
+          Assert.Equal(4, 2+2);
+      }
+
+      [Fact]
+      public void HelloTest()
+      {
+          Assert.Equal("hello", new Echo().echo("hello"));
+      }
+
+    }
+}
